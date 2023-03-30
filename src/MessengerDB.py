99343fbb5e6e0c36f8bff94b6ec0d8c1d534d44a
Samuel <ssoderbe@kth.se>
2023-03-30 18:19:32 +0200

    fix: Changes attribute names

--- src/MessengerDB.py
+++ src/MessengerDB.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import sqlite3
 import time
 import uuid
-from src.models.models import Message
+from src.models.models import Message, MessagePatch
 from result import Ok, Err, Result
 from sqlite3 import OperationalError
 from pypika import Query, Table, Column
@@ -41,9 +41,9 @@ class MessengerDB:
                 .create_table(self.table_name) \
                 .if_not_exists() \
                 .columns(Column('id', 'STRING', nullable=False),
-                         Column('user_id', 'STRING', nullable=False),
+                         Column('recipient_id', 'STRING', nullable=False),
                          Column('timestamp', 'STRING', nullable=False),
-                         Column('sender', 'STRING', nullable=False),
+                         Column('sender_id', 'STRING', nullable=False),
                          Column('message', 'STRING', nullable=False)
                          ) \
                 .primary_key('id')
@@ -64,10 +64,10 @@ class MessengerDB:
         except OperationalError as err:
             return Err(f'Failed to close connection to table {self.table_name}, got error: {err}')
 
-    def insert_message(self, user_id, sender_id, message, date_sent=None) -> Result[str, str]:
+    def insert_message(self, recipient_id, sender_id, message, date_sent=None) -> Result[str, str]:
         """
         Creates uuid and inserts message into database
-        :param user_id: String, id of recipient
+        :param recipient_id: String, id of recipient
         :param sender_id: String, id of sender
         :param message: String, message
         :param date_sent: String, date message was sent (mainly for testing)
@@ -78,7 +78,7 @@ class MessengerDB:
 
         command = Query \
             .into(self.table_name) \
-            .insert(message_id, user_id, timestamp, sender_id, message)
+            .insert(message_id, recipient_id, timestamp, sender_id, message)
 
         try:
             self.connection.execute(command.get_sql())
@@ -87,13 +87,13 @@ class MessengerDB:
         except OperationalError as err:
             return Err(f'Failed to add message to table {self.table_name}, got error: {err}')
 
-    def select_from_user(self, user_id, from_date=None, to_date=None) -> Result[list[Message], str]:
+    def select_from_recipient(self, recipient_id, from_date=None, to_date=None) -> Result[list[Message], str]:
         """
-        Selects entries from the table for the given user between given dates
-        :param user_id: String, id of user
+        Selects entries from the table for the given recipient between given dates
+        :param recipient_id: String, id of recipient
         :param from_date: String, start date on format yyyy-mm-dd, epoch if not provided
         :param to_date: String, end date on format yyyy-mm-dd, current time if not provided
-        :return: Result with list of rows (id, user_id, timestamp, sender, message) and string if failed
+        :return: Result with list of rows (id, recipient_id, timestamp, sender_id, message) and string if failed
         """
         from_timestamp = convert_to_timestamp(from_date) if from_date else 0
         to_timestamp = convert_to_timestamp(to_date, end_of_day=True) if to_date else time.time()
@@ -101,8 +101,8 @@ class MessengerDB:
         table = Table(self.table_name)
         command = Query \
             .from_(self.table_name) \
-            .select('id', 'user_id', 'timestamp', 'sender', 'message') \
-            .where((table.user_id == user_id) & (table.timestamp[from_timestamp:to_timestamp])) \
+            .select('id', 'recipient_id', 'timestamp', 'sender_id', 'message') \
+            .where((table.recipient_id == recipient_id) & (table.timestamp[from_timestamp:to_timestamp])) \
             .orderby('timestamp')
 
         try:
@@ -114,7 +114,7 @@ class MessengerDB:
         except OperationalError as err:
             return Err(f'Failed to select from table {self.table_name}, got error: {err}')
 
-    def delete_messages(self, message_ids) -> Result[str, str]:
+    def delete_messages(self, message_ids: str) -> Result[str, str]:
         """
         Deletes message with given id from table.
         :param message_ids: List of strings, ids of messages to delete
@@ -136,11 +136,11 @@ class MessengerDB:
     def get_all(self) -> Result[list[Message], str]:
         """
         Fetches all message in DB (mainly for testing)
-        :return: Result with list of rows (id, user_id, timestamp, sender, message) and string if failed
+        :return: Result with list of rows (id, recipient_id, timestamp, sender_id, message) and string if failed
         """
         command = Query \
             .from_(self.table_name) \
-            .select('id', 'user_id', 'timestamp', 'sender', 'message')
+            .select('id', 'recipient_id', 'timestamp', 'sender_id', 'message')
 
         try:
             cursor = self.connection.cursor()
