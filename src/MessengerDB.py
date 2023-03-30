45e4d21e0a3a67e864ce6260795f04b1749ad896
Samuel <ssoderbe@kth.se>
2023-03-30 14:23:42 +0200

    feat: Changes to SQLite DB. Includes insert, delete, select methods

--- src/MessengerDB.py
+++ src/MessengerDB.py
@@ -1,58 +1,137 @@
+from datetime import datetime
+import sqlite3
 import time
-import boto3
-from botocore.exceptions import ClientError
+import uuid
+from sqlite3 import OperationalError
+
+from pypika import Query, Table, Column
+
+
+def convert_to_timestamp(date, end_of_day=False):
+    """
+    Converts date in yyyy-mm-dd format to timestamp (seconds since epoch).
+    :param date: String on format yyyy-mm-dd
+    :param end_of_day: Boolean, if timestamp should be at end of day
+    :return: Timestamp as float
+    """
+    # To get timestamp at end of day
+    end_time = {
+        'hour': 23,
+        'minute': 59,
+        'second': 59
+    }
+    return datetime.timestamp(datetime(*map(int, date.split('-')), **end_time if end_of_day else {}))
 
 
 class MessengerDB:
 
-    def __init__(self, resource):
-        self.resource = resource
-        self.table_name = None
-        self.table = None
+    def __init__(self, table_name):
+        self.table_name = table_name
+        self.connection = None
 
-    def create_table(self, table_name: str):
+    def connect(self):
+        """
+        Establishes connection to database, creates new if not exists.
+        :return: None
+        """
         try:
-            table = self.resource.create_table(
-                TableName=table_name,
-                KeySchema=[
-                    {'AttributeName': 'user_id', 'KeyType': 'HASH'},
-                    {'AttributeName': 'timestamp', 'KeyType': 'RANGE'}
-                ],
-                AttributeDefinitions=[
-                    {'AttributeName': 'user_id', 'AttributeType': 'S'},
-                    {'AttributeName': 'timestamp', 'AttributeType': 'N'},
-                ]
-            )
-            table.wait_until_exists()
-        except ClientError as e:
-            print(f'Failed to create table {table_name}, error: {e}')
-        else:
-            self.table_name = table_name
-            self.table = table
-            return self.table
+            self.connection = sqlite3.connect(self.table_name)
+            command = Query \
+                .create_table(self.table_name) \
+                .if_not_exists() \
+                .columns(Column('id', 'STRING', nullable=False),
+                         Column('user_id', 'STRING', nullable=False),
+                         Column('timestamp', 'STRING', nullable=False),
+                         Column('sender', 'STRING', nullable=False),
+                         Column('message', 'STRING', nullable=False)
+                         ) \
+                .primary_key('id')
+            self.connection.execute(command.get_sql())
+            print(f'Connected to table {self.table_name}')
+        except OperationalError as err:
+            print(f'Failed to create table {self.table_name}, got error: {err}')
 
-    def delete_table(self):
+    def close(self):
+        """
+        Closes connection to table.
+        :return: None
+        """
         try:
-            self.table.delete()
-            self.table = None
-        except ClientError as e:
-            print(f'Failed to delete table {self.table_name}, error: {e}')
+            self.connection.close()
+            self.connection = None
+            print(f'Connection to {self.table_name} closed')
+        except OperationalError as err:
+            print(f'Failed to close connection to table {self.table_name}, got error: {err}')
 
-    def add_message(self, recipient_id, sender_id, message):
-        now = time.time()
+    def insert_message(self, user_id, sender_id, message, date_sent=None):
+        """
+        Creates uuid and inserts message into database
+        :param user_id: String, id of recipient
+        :param sender_id: String, id of sender
+        :param message: String, message
+        :param date_sent: String, date message was sent (mainly for testing)
+        :return: None
+        """
+        timestamp = convert_to_timestamp(date_sent) if date_sent else time.time()
+        message_id = uuid.uuid4().hex
 
-        try:
-            self.table.put_item(
-                Item={
-                    'user_id': recipient_id,
-                    'timestamp': now,
-                    'data': {
-                        'sender': sender_id,
-                        'message': message,
-                    },
-                }
-            )
-        except ClientError as e:
-            print(f'Failed to add item to table {self.table_name}, error: {e}')
+        command = Query \
+            .into(self.table_name) \
+            .insert(message_id, user_id, timestamp, sender_id, message)
 
+        self.connection.execute(command.get_sql())
+        self.connection.commit()
 
+    def select_from_user(self, user_id, from_date=None, to_date=None):
+        """
+        Selects entries from the table for the given user between given dates
+        :param user_id: String, id of user
+        :param from_date: String, start date on format yyyy-mm-dd, epoch if not provided
+        :param to_date: String, end date on format yyyy-mm-dd, current time if not provided
+        :return: List of rows (id, user_id, timestamp, sender, message)
+        """
+        from_timestamp = convert_to_timestamp(from_date) if from_date else 0
+        to_timestamp = convert_to_timestamp(to_date, end_of_day=True) if to_date else time.time()
+
+        table = Table(self.table_name)
+        command = Query \
+            .from_(self.table_name) \
+            .select('id', 'user_id', 'timestamp', 'sender', 'message') \
+            .where((table.user_id == user_id) & (table.timestamp[from_timestamp:to_timestamp])) \
+            .orderby('timestamp')
+
+        cursor = self.connection.cursor()
+        cursor.execute(command.get_sql())
+        res = cursor.fetchall()
+        cursor.close()
+        return res
+
+    def delete_message(self, message_id):
+        """
+        Deletes message with given id from table.
+        :param message_id: String, id of message to delete
+        :return: None
+        """
+        table = Table(self.table_name)
+        command = Query \
+            .from_(self.table_name) \
+            .where(table.id == message_id) \
+            .delete()
+
+        self.connection.execute(command.get_sql())
+        self.connection.commit()
+
+    def get_all(self):
+        """
+        Fetches all message in DB (mainly for testing)
+        :return: List of rows (id, user_id, timestamp, sender, message)
+        """
+        command = Query \
+            .from_(self.table_name) \
+            .select('id', 'user_id', 'timestamp', 'sender', 'message')
+
+        cursor = self.connection.cursor()
+        cursor.execute(command.get_sql())
+        res = cursor.fetchall()
+        cursor.close()
+        return res
