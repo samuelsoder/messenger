b684a01b0eadda2a8d87448c61a525c5a322c363
Samuel <ssoderbe@kth.se>
2023-03-31 10:02:29 +0200

    fix: Raise exception if db not connected

--- src/bootstraper.py
+++ src/bootstraper.py
@@ -7,6 +7,10 @@ def init_db():
     :return: MessengerDB instance
     """
     db = MessengerDB('messenger')
-    db.connect()
-
-    return db
+    connecting = db.connect()
+    if connecting.is_ok():
+        print(connecting.value)
+        return db
+    else:
+        print(connecting.value)
+        raise Exception
